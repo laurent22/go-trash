c0004219bf6c0c96acc138d2f0b9156b03a1aed4
Laurent Cozic <laurent@pogopixels.com>
2014-03-09 03:41:51 +0800

    Added support Windows

---
+++ recycle.h
@@ -0,0 +1,4 @@
+int RecycleFiles (char** filenames, int nFiles, int bConfirmed);
+char** makeCharArray(int size);
+void setArrayString(char **a, char *s, int n);
+void freeCharArray(char **a, int size);
---
+++ trash_windows.go
@@ -0,0 +1,28 @@
+package trash
+
+/*
+#include "recycle.h"
+*/
+import "C"
+
+import (
+	"errors"
+	"fmt"
+)
+
+func MoveToTrash(filePath string) (string, error) {
+	fmt.Println("here")
+
+	files := []string{filePath}
+	C_files := C.makeCharArray(C.int(len(files)))
+	defer C.freeCharArray(C_files, C.int(len(files)))
+	for i, s := range files {
+		C.setArrayString(C_files, C.CString(s), C.int(i))
+	}
+
+	success := C.RecycleFiles(C_files, C.int(len(files)), C.int(0))
+	if success != 1 {
+		return "", errors.New("file could not be recycled")
+	}
+	return "", nil
+}
