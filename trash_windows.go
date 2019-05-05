3c8fdcc57de0f026fc680ae4a5e38ea41f3317f3
xoviat <xoviat@users.noreply.github.com>
2019-05-05 09:44:11 -0500

    simplify windows implementation

--- trash_windows.go
+++ trash_windows.go
@@ -5,7 +5,7 @@ package trash
 import (
 	"errors"
 	"strconv"
-	"syscall"
+	"unicode/utf16"
 
 	"github.com/lxn/win"
 )
@@ -15,45 +15,21 @@ func IsAvailable() bool {
 	return true
 }
 
-func getShortPathName(path string) (string, error) {
-	p, err := syscall.UTF16FromString(path)
-	if err != nil {
-		return "", err
-	}
-	b := p // GetShortPathName says we can reuse buffer
-	n := uint32(len(b))
-	for {
-		n, err = syscall.GetShortPathName(&p[0], &b[0], uint32(len(b)))
-		if err != nil {
-			return "", err
-		}
-		if n <= uint32(len(b)) {
-			return syscall.UTF16ToString(b[:n]), nil
-		}
-		b = make([]uint16, n)
-	}
+func DoubleNullTerminatedUTF16PtrFromString(s string) *uint16 {
+	return &(utf16.Encode([]rune(s + "\x00\x00"))[0])
 }
 
 func MoveToTrash(filePath string) (string, error) {
-
-	filePath, err := getShortPathName(filePath)
-	if err != nil {
-		return "", err
-	}
-
-	fileop := win.SHFILEOPSTRUCT{
+	if result := win.SHFileOperation(&win.SHFILEOPSTRUCT{
 		Hwnd:                  win.HWND(0),
 		WFunc:                 win.FO_DELETE,
-		PFrom:                 syscall.StringToUTF16Ptr(filePath),
+		PFrom:                 DoubleNullTerminatedUTF16PtrFromString(filePath),
 		PTo:                   nil,
 		FFlags:                win.FOF_ALLOWUNDO | win.FOF_NOCONFIRMATION | win.FOF_NOERRORUI | win.FOF_SILENT,
 		FAnyOperationsAborted: win.BOOL(0),
 		HNameMappings:         0,
-		LpszProgressTitle:     syscall.StringToUTF16Ptr(""),
-	}
-
-	result := win.SHFileOperation(&fileop)
-	if result != 0 {
+		LpszProgressTitle:     DoubleNullTerminatedUTF16PtrFromString(""), // Note: double-null termination not required
+	}); result != 0 {
 		return "", errors.New("File operation returned code " + strconv.Itoa(int(result)))
 	}
 
